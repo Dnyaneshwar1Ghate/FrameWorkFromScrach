07af3f9fa4e774253b1f4b8d01de94875393c17d
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2024-11-14 00:51:03 +0530

    updated code

---
+++ src/main/java/com/tmb/constants/FrameworkConstants.java
@@ -0,0 +1,10 @@
+package com.tmb.constants;
+
+public final class FrameworkConstants {
+	private static final String firefoxPath="F:\\Software\\geckodriver-v0.32.2-win32\\geckodriver.exe"; 
+
+	public static String getFirepath()
+	{
+		return firefoxPath;
+	}
+}
