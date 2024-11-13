dae1809b3103ae269bf0c754e3bb5f176a3c4c2a
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2024-11-14 01:32:16 +0530

    update all classes

---
+++ src/main/java/com/tbb/driver/Driver.java
@@ -0,0 +1,23 @@
+package com.tbb.driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.tmb.constants.FrameworkConstants;
+
+public class Driver {
+	
+	public static WebDriver driver;
+	public static void initDriver()
+	{
+		System.setProperty("webdriver.gecko.driver",FrameworkConstants.getFirepath());
+		driver = new FirefoxDriver();
+		driver.get("https://google.com");
+	}
+	
+	public static void quitDriver()
+	{
+		driver.quit();
+	}
+
+}
