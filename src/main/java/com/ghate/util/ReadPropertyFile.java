c6d1632cc9f8963f412a3eeff531edbb25016404
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2024-11-19 00:10:31 +0530

    add static block in property file

--- src/main/java/com/ghate/util/ReadPropertyFile.java
+++ src/main/java/com/ghate/util/ReadPropertyFile.java
@@ -1,6 +1,8 @@
 package com.ghate.util;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 
 public final class ReadPropertyFile {
@@ -9,12 +11,24 @@ public final class ReadPropertyFile {
 	{
 
 	}
+	private static Properties property=new Properties();
 
+	static {
+		try
+		{
+			FileInputStream file=new FileInputStream(System.getProperty("user.dir")+"/src/test/resources/config/config.properties");
+			property.load(file);
+		}catch(FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
 	public static String getValue(String key1) throws Exception
 	{
-		Properties property=new Properties();
-		FileInputStream file=new FileInputStream(System.getProperty("user.dir")+"/src/test/resources/config/config.properties");
-		property.load(file);
 		String value=property.getProperty(key1);
 		if(value==null)
 		{
