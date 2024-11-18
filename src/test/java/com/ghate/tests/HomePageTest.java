e5d077ba6c71f41912f3991a0f57efaae597e5ed
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2024-11-19 02:44:21 +0530

    testcase 1

--- src/test/java/com/ghate/tests/HomePageTest.java
+++ src/test/java/com/ghate/tests/HomePageTest.java
@@ -1,10 +1,15 @@
 package com.ghate.tests;
 
+import java.util.List;
+import java.util.Objects;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import com.ghate.driver.Driver;
+
 import com.ghate.driver.DriverManager;
 
 public final class HomePageTest extends BaseTest {
@@ -19,6 +24,35 @@ public final class HomePageTest extends BaseTest {
 		DriverManager.getDriver().findElement(By.name("q")).sendKeys("All About Testing",Keys.ENTER);
 		Thread.sleep(3000);
 	}*/
+	@Test
+	public void TestCase1()
+	{
+		DriverManager.getDriver().findElement(By.name("q")).sendKeys("Testing mini bytes - Youtube",Keys.ENTER);
+		String title=DriverManager.getDriver().getTitle();
+		Assert.assertTrue(Objects.nonNull(title));
+		Assert.assertTrue(title.toLowerCase().contains("google search"));
+		Assert.assertTrue(title.toLowerCase().matches("\\w.*"+"gooel.com search"));
+		Assert.assertTrue(title.length()>15);
+		Assert.assertTrue(title.length()<100);
+		List<WebElement> elements=DriverManager.getDriver().findElements(By.xpath("//h3/spam"));
+		Assert.assertEquals(elements.size(),10);
+		
+		boolean isElementPresetn=false;
+		
+		for(WebElement elem : elements)
+		{
+			
+			if(elem.getText().equalsIgnoreCase("Testing mini bytes -youtube"))
+			{
+				isElementPresetn=true;
+				break;
+			}
+			
+		}
+		Assert.assertTrue(isElementPresetn,"Testing mini bytes -youtube");
+		
+		
+	}
 	
 	
 	
