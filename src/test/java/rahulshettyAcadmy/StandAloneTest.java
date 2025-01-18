f5e54e09e331c0c59ac889a4ff026fba8d6ac25f
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2025-01-19 00:40:44 +0530

    Done

---
+++ src/test/java/rahulshettyAcadmy/StandAloneTest.java
@@ -0,0 +1,37 @@
+package rahulshettyAcadmy;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class StandAloneTest {
+	public static void main(String[] args) {
+
+			WebDriverManager.chromedriver().setup();
+			WebDriver driver=new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver.get("https://rahulshettyacademy.com/client");
+			
+			driver.findElement(By.id("userEmail")).sendKeys("dnyaneshwarghate1010@gmail.com");
+			driver.findElement(By.id("userPassword")).sendKeys("Dghate@2025");
+			
+			driver.findElement(By.id("login")).click();
+			
+			List<WebElement> products =driver.findElements(By.cssSelector(".mb-3"));
+			System.out.println(products.size());
+			WebElement pro=products.stream().filter(product->product.findElement(By.cssSelector("b")).getText().equals("ZARA COAT 3")).findFirst().orElse(null);
+			
+			
+			
+		
+		
+	}
+
+}
